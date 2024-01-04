5750ed3d4854eeaca27a33f6e9892a19417f7847
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-04 15:11:43 +0100

    feat: add GIF script

---
+++ GIF.py
@@ -0,0 +1,13 @@
+import glob
+import imageio.v2 as imageio
+gan_dir = 'birs_64'
+
+anim_file = f'Gan_Tut/plots/{gan_dir}/gif.gif'
+with imageio.get_writer(anim_file, mode='I') as writer:
+  filenames = glob.glob(f'Gan_Tut/plots/{gan_dir}/image*.png')
+  filenames = sorted(filenames)
+  for filename in filenames:
+    image = imageio.imread(filename)
+    writer.append_data(image)
+  image = imageio.imread(filename)
+  writer.append_data(image)
