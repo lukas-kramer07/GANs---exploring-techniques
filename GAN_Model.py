df9fd9d08a3d2083584e40b5fb6712f428ad5353
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-07 09:31:55 +0100

    innit: add models

---
+++ GAN_Model.py
@@ -0,0 +1,88 @@
+'''
+GAN as a custom model subclass with custom training step
+'''
+
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import os
+from keras import layers
+import time
+
+gan_dir = "celeb_64"
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(8 * 8 * 256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((8, 8, 256)))
+    assert model.output_shape == (None, 8, 8, 256)  # Note: None is the batch size
+
+    model.add(
+        layers.Conv2DTranspose(
+            128, (5, 5), strides=(2, 2), padding="same", use_bias=False
+        )
+    )
+    assert model.output_shape == (None, 16, 16, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(
+        layers.Conv2DTranspose(
+            128, (5, 5), strides=(2, 2), padding="same", use_bias=False
+        )
+    )
+    assert model.output_shape == (None, 32, 32, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(
+        layers.Conv2DTranspose(
+            64, (5, 5), strides=(2, 2), padding="same", use_bias=False
+        )
+    )
+    assert model.output_shape == (None, 64, 64, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(
+        layers.Conv2DTranspose(
+            3,
+            (5, 5),
+            strides=(1, 1),
+            padding="same",
+            use_bias=False,
+            activation="sigmoid",
+        )
+    )
+    assert model.output_shape == (None, 64, 64, 3)
+
+    return model
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(
+        layers.Conv2D(
+            64, (5, 5), strides=(2, 2), padding="valid", input_shape=[64, 64, 3]
+        )
+    )
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding="valid"))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding="valid"))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
