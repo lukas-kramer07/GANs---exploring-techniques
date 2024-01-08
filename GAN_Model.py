4f6fed9850c3b8a539d6beb3f8656bac9d16c610
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-08 06:51:01 +0100

    refactor: increase latent dim size

--- GAN_Model.py
+++ GAN_Model.py
@@ -10,48 +10,31 @@ from keras import layers
 from keras.losses import BinaryCrossentropy
 
 gan_dir = "num_28"
-LATENT_DIM = 100
+LATENT_DIM = 128
 EPOCHS = 3000
 num_examples_to_generate = 16
 BATCH_SIZE = 512
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(LATENT_DIM,)))
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(LATENT_DIM,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((7, 7, 256)))
     assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
 
-    model.add(
-        layers.Conv2DTranspose(
-            128, (5, 5), strides=(2, 2), padding="same", use_bias=False
-        )
-    )
-    assert model.output_shape == (None, 14, 14, 128)
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(
-        layers.Conv2DTranspose(
-            64, (5, 5), strides=(2, 2), padding="same", use_bias=False
-        )
-    )
-    assert model.output_shape == (None, 28, 28, 64)
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(
-        layers.Conv2DTranspose(
-            1,
-            (5, 5),
-            strides=(1, 1),
-            padding="same",
-            use_bias=False,
-            activation="tanh",
-        )
-    )
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     assert model.output_shape == (None, 28, 28, 1)
 
     return model
@@ -59,15 +42,12 @@ def make_generator_model():
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(
-        layers.Conv2D(
-            64, (5, 5), strides=(2, 2), padding="valid", input_shape=[28, 28, 1]
-        )
-    )
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding="valid"))
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
