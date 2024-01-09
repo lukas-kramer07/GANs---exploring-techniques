02ead589d1b5aba349398dcf00a2c2e9fff015f4
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-09 18:53:06 +0100

    chore: clean up logs

--- test2.py
+++ test2.py
@@ -1,3 +1,17 @@
 import tensorflow as tf
-gpus = tf.config.list_physical_devices("GPU")
-print(gpus)
+import keras
+# Define the input label
+input_label = tf.constant([0], dtype=tf.int32)
+
+# Define the size of the embedding space
+embedding_dim = 4
+
+
+# Apply the embedding layer to the input label
+embedded_output = keras.layers.Embedding(10, 50)(input_label)
+
+# Print the original label and the corresponding embedded representation
+print("Original Label:")
+print(input_label.numpy())
+print("\nEmbedded Representation:")
+print(embedded_output.numpy())
