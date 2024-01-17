1214ca063ae4a4e56e011cb03221f1fcf1921c90
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-17 21:08:19 +0100

    test: add gif

--- GIF.py
+++ GIF.py
@@ -1,6 +1,6 @@
 import glob
 import imageio.v2 as imageio
-gan_dir = 'fashion_28'
+gan_dir = 'cifar_32'
 
 anim_file = f'Gan_Tut/plots/{gan_dir}/gif.gif'
 with imageio.get_writer(anim_file, mode='I') as writer:
