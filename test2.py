24831e56bc34ba8849a3d5903bcd81580bf2a198
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-01-28 09:15:17 +0100

    feat: add wasserstein test

--- test2.py
+++ test2.py
@@ -1,17 +1,21 @@
 import tensorflow as tf
-import keras
-# Define the input label
-input_label = tf.constant([0], dtype=tf.int32)
+from keras import backend
 
-# Define the size of the embedding space
-embedding_dim = 4
+input = tf.random.normal([80,1000])
 
+print(backend.mean(input) == tf.reduce_mean(input))
 
-# Apply the embedding layer to the input label
-embedded_output = keras.layers.Embedding(10, 50)(input_label)
+#first implementation
+y_true1 = tf.constant([-1]*5)
+y_pred1= tf.constant([-3,-4,-5,-6,-7])*-1
 
-# Print the original label and the corresponding embedded representation
-print("Original Label:")
-print(input_label.numpy())
-print("\nEmbedded Representation:")
-print(embedded_output.numpy())
+y_true2 = tf.constant([1]*5)
+y_pred2 = tf.constant([-2,-3,-5,-6,0])
+
+print(tf.reduce_mean(y_pred1*y_true1)+tf.reduce_mean(y_pred2*y_true2))
+
+# second implementation
+y_true = y_pred1
+y_pred = y_pred2
+print(tf.reduce_mean(y_true), tf.reduce_mean(y_pred))
+print(-(tf.reduce_mean(y_true)-tf.reduce_mean(y_pred)))
